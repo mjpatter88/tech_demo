261d260fb23bf77cb401fa6d70bbf296dc561979
Michael Patterson <mjpatter88@gmail.com>
2020-01-13 21:38:05 -0600

    Add support for recording, uploading, downloading, and playing audio files.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # tech_demo
 
 A new Flutter project to spike out needed features.
+
+Add the firebase project google-services.json files and `flutter run`.
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'dart:io';
 
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_sound/flutter_sound.dart';
 
 const String fileName = "TestFile.txt";
+const String uploadedAudioFileName = "TestRecording.aac";
+const String downloadedAudioFileName = "TestRecording.aac";
 const String testString = 'Hello files!';
 
 void main() {
@@ -33,23 +36,41 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  FlutterSound flutterSound;
+  bool recording = false;
   bool uploading = false;
   bool downloading = false;
+  bool uploadingRecording = false;
+  bool downloadingRecording = false;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  String audioFilePath = "";
+
+  @override
+  void initState() {
+    super.initState();
+    flutterSound = FlutterSound();
+  }
+
+  @override
+  void dispose() {
+    flutterSound.stopRecorder();
+    flutterSound.stopPlayer();
+    super.dispose();
   }
 
   Future<void> _uploadTextFile() async {
+    print("Uploading...");
+    setState(() {
+      uploading = true;
+    });
+
     final Directory systemTempDir = Directory.systemTemp;
     final File file = await File('${systemTempDir.path}/$fileName').create();
     await file.writeAsString(testString);
     assert(await file.readAsString() == testString);
 
-    final StorageReference storageRef = FirebaseStorage.instance.ref().child(fileName);
+    final StorageReference storageRef =
+        FirebaseStorage.instance.ref().child(fileName);
     final StorageUploadTask uploadTask = storageRef.putFile(
       file,
       StorageMetadata(
@@ -57,10 +78,6 @@ class _MyHomePageState extends State<MyHomePage> {
         customMetadata: <String, String>{'activity': 'test'},
       ),
     );
-    print("Uploading...");
-    setState(() {
-      uploading = true;
-    });
     StorageTaskSnapshot uploadTaskSnap = await uploadTask.onComplete;
     String url = await uploadTaskSnap.ref.getDownloadURL();
     print("Complete! $url");
@@ -70,6 +87,11 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<void> _downloadTextFile() async {
+    print("Downloading...");
+    setState(() {
+      downloading = true;
+    });
+
     final Directory systemTempDir = Directory.systemTemp;
     final File tempFile = File('${systemTempDir.path}/$fileName');
     if (tempFile.existsSync()) {
@@ -78,13 +100,10 @@ class _MyHomePageState extends State<MyHomePage> {
     await tempFile.create();
     assert(await tempFile.readAsString() == "");
 
-    final StorageReference storageRef = FirebaseStorage.instance.ref().child(fileName);
-    final StorageFileDownloadTask downloadTask = storageRef.writeToFile(tempFile);
-
-    print("Downloading...");
-    setState(() {
-      downloading = true;
-    });
+    final StorageReference storageRef =
+        FirebaseStorage.instance.ref().child(fileName);
+    final StorageFileDownloadTask downloadTask =
+        storageRef.writeToFile(tempFile);
 
     final int byteCount = (await downloadTask.future).totalByteCount;
     final String tempFileContents = await tempFile.readAsString();
@@ -97,6 +116,96 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _recordPressed() {
+    if (recording) {
+      _stopRecording();
+    } else {
+      _startRecording();
+    }
+  }
+
+  Future<void> _startRecording() async {
+    print("Recording...");
+    setState(() {
+      recording = true;
+    });
+    audioFilePath = await flutterSound.startRecorder(
+      "sound.aac",
+      codec: t_CODEC.CODEC_AAC,
+    );
+    print("Path: $audioFilePath");
+  }
+
+  Future<void> _stopRecording() async {
+    print("Recording Stopped.");
+    setState(() {
+      recording = false;
+    });
+
+    String result = await flutterSound.stopRecorder();
+    print("Result: $result");
+  }
+
+  Future<void> _uploadRecording() async {
+    assert(audioFilePath != null);
+    print("Uploading Recordng...");
+    setState(() {
+      uploadingRecording = true;
+    });
+
+    final File file = File(audioFilePath);
+    assert(await file.exists());
+
+    final StorageReference storageRef =
+        FirebaseStorage.instance.ref().child(uploadedAudioFileName);
+    final StorageUploadTask uploadTask = storageRef.putFile(file);
+    StorageTaskSnapshot uploadTaskSnap = await uploadTask.onComplete;
+    String url = await uploadTaskSnap.ref.getDownloadURL();
+    print("Complete! $url");
+    setState(() {
+      uploadingRecording = false;
+    });
+  }
+
+  Future<void> _downloadRecording() async {
+    print("Downloading Recording...");
+    setState(() {
+      downloadingRecording = true;
+    });
+
+    final Directory systemTempDir = Directory.systemTemp;
+    final File audioFile =
+        File('${systemTempDir.path}/$downloadedAudioFileName');
+    if (audioFile.existsSync()) {
+      await audioFile.delete();
+    }
+    await audioFile.create();
+    assert(await audioFile.readAsString() == "");
+
+    final StorageReference storageRef =
+        FirebaseStorage.instance.ref().child(uploadedAudioFileName);
+    final StorageFileDownloadTask downloadTask =
+        storageRef.writeToFile(audioFile);
+
+    final int byteCount = (await downloadTask.future).totalByteCount;
+
+    print("Complete! $byteCount bytes");
+    setState(() {
+      downloadingRecording = false;
+    });
+  }
+
+  Future<void> _playAudio() async {
+    print("Playing Audio.");
+    final Directory systemTempDir = Directory.systemTemp;
+    final String downloadedAudioFileUri =
+        '${systemTempDir.path}/$downloadedAudioFileName';
+    assert(await File(downloadedAudioFileUri).exists());
+    String result = await flutterSound.startPlayer(downloadedAudioFileUri);
+    print("Result: $result");
+    String volumeResult = await flutterSound.setVolume(1.0);
+    print("Volume Set Result: $volumeResult");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -108,48 +217,57 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            // TODO: start here and implement the following actions
+            Container(
+              width: 200,
+              child: RaisedButton(
+                onPressed: _recordPressed,
+                child:
+                    recording ? Text('Stop Recording') : Text('Record Audio'),
+              ),
+            ),
+            Container(
+              width: 200,
+              child: RaisedButton(
+                onPressed: _uploadRecording,
+                child: uploadingRecording
+                    ? LinearProgressIndicator()
+                    : Text('Upload Recording'),
+              ),
+            ),
+            Container(
+              width: 200,
+              child: RaisedButton(
+                onPressed: _downloadRecording,
+                child: downloadingRecording
+                    ? LinearProgressIndicator()
+                    : Text('Download Audio'),
+              ),
+            ),
+            Container(
+              width: 200,
+              child: RaisedButton(
+                onPressed: _playAudio,
+                child: Text('Play Audio'),
+              ),
+            ),
+            SizedBox(height: 50),
             Container(
               width: 200,
               child: RaisedButton(
                 onPressed: _uploadTextFile,
-                child: uploading ? LinearProgressIndicator(): Text('Upload Text File'),
+                child: uploading
+                    ? LinearProgressIndicator()
+                    : Text('Upload Text File'),
               ),
             ),
             Container(
               width: 200,
               child: RaisedButton(
                 onPressed: _downloadTextFile,
-                child: downloading ? LinearProgressIndicator(): Text('Download Text File'),
-              ),
-            ),
-            SizedBox(height: 30,),
-            // TODO: start here and implement the following actions
-            Container(
-              width: 200,
-              child: RaisedButton(
-                onPressed: () => print('Recording not implemented yet...'),
-                child: Text('Record Audio'),
-              ),
-            ),
-            Container(
-              width: 200,
-              child: RaisedButton(
-                onPressed: () => print('Uploading not implemented yet...'),
-                child: Text('Upload Recording'),
-              ),
-            ),
-            Container(
-              width: 200,
-              child: RaisedButton(
-                onPressed: () => print('Downloading not implemented yet...'),
-                child: Text('Download Audio'),
-              ),
-            ),
-            Container(
-              width: 200,
-              child: RaisedButton(
-                onPressed: () => print('Playing Audio not implemented yet...'),
-                child: Text('Play Audio'),
+                child: downloading
+                    ? LinearProgressIndicator()
+                    : Text('Download Text File'),
               ),
             ),
           ],
